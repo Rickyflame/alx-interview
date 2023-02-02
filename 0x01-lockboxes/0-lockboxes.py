31bca1ebfbd2caaf777c5e6ec9629436affc46ed
Rickyflame <kimumuricky@gmail.com>
2023-02-02 17:31:45 +0300

    task 0

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,15 @@
+def canUnlockAll(boxes):
+    visited = [False] * len(boxes)
+    visited[0] = True
+    stack = [0]
+
+
+    while stack:
+        box = stack.pop()
+        for key in boxes[box]:
+            if key < len(boxes) and not visited[key]:
+                visited[key] = True
+                stack.append(key)
+
+
+    return all(visited)
