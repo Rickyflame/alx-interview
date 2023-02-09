3645907e53c048d0f418c2a7b374d27a780cc454
Rickyflame <kimumuricky@gmail.com>
2023-02-09 22:06:15 +0300

    task 0

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -7,35 +7,32 @@ def minOperations(n):
     calculate the fewest number of operations
     needed to result in exactly n H
     '''
-    char_count = 1 #number of chars in file
-    H_count = 0 #number of H copied
-    cnt = 0 #operation count
+    char_count = 1
+    h_count = 0
+    cnt = 0
 
     while char_count < n:
-        if H_count == 0:
-            H_count = char_count
+        if h_count == 0:
+            h_count = char_count
             cnt += 1
 
         if char_count == 1:
-            char_count += H_count
+            char_count += h_count
             cnt += 1
-            continue
 
         remaining = n - char_count
-        if remaining < H_count:
+        if remaining < h_count:
             return 0
 
         if remaining % char_count != 0:
-            char_count += H_count
+            char_count += h_count
             cnt += 1
-
         else:
-            H_count = char_count
-            char_count += H_count
+            h_count = char_count
             cnt += 2
 
     if char_count == n:
         return cnt
     else:
         return 0
-        
+
